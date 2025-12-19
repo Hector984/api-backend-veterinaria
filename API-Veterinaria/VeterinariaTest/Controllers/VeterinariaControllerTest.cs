935aa764e8c91b7b4b604f59224c1cc88b4ed4f1
Hector984 <hectoruam96@gmail.com>
2025-12-19 10:15:22 -0600

    Configuracion de pruebas con MSTest y NSubstitute para Mocks
    Primer prueba del controlador Veterinarias

---
+++ API-Veterinaria/VeterinariaTest/Controllers/VeterinariaControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using API_Veterinaria.Business.Interfaces;
+using API_Veterinaria.Business.Services;
+using API_Veterinaria.Controllers;
+using API_Veterinaria.Core.DTOs.Cliente;
+using API_Veterinaria.Core.Entities;
+using API_Veterinaria.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinariaTest.Utilities;
+
+namespace VeterinariaTest.Controllers
+{
+    [TestClass]
+    public class VeterinariaControllerTest: BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerVeterinariaPorId_Retorna404()
+        {
+            // Preparamos las dependencias que requiere el controlador Veterinarias
+            var nombreBD = Guid.NewGuid().ToString();
+            var context = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+            var veterinariaRepository = new VeterinariaRepository(context);
+
+            var usuarioService = Substitute.For<IUsuarioService>();
+
+            usuarioService.GetUsuarioByEmail()
+                .Returns((Usuario)null);
+
+            var veterinariaService = new VeterinariaService(
+                veterinariaRepository,
+                usuarioService, 
+                mapper
+               );
+
+            var controller = new VeterinariasController(veterinariaService);
+
+            // Ejecutamos la prueba
+            var respuesta = await controller.ObtenerVeterinariaPorId(1);
+
+            // Verificamos el resultado
+            Assert.IsNotNull(respuesta);
+            Assert.IsInstanceOfType(respuesta.Result, typeof(NotFoundResult));
+            //var resultado = respuesta.Result as StatusCodeResult;
+            //Assert.AreEqual(expected: 404, actual: resultado!.StatusCode);
+        }
+    }
+}
---
+++ API-Veterinaria/VeterinariaTest/Utilities/BasePruebas.cs
@@ -0,0 +1,35 @@
+﻿using API_Veterinaria.Data;
+using API_Veterinaria.Utilities;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinariaTest.Utilities
+{
+    public class BasePruebas
+    {
+        protected VeterinariaDbContext ConstruirContext(string nombreDB)
+        {
+            var opciones = new DbContextOptionsBuilder<VeterinariaDbContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+
+            var dbContext = new VeterinariaDbContext(opciones);
+
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(opciones =>
+            {
+                opciones.AddProfile(new AutoMapperProfiles());
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
